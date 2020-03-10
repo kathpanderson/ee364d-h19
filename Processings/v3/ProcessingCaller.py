9369e8f6685c0c8ad4282477282b044771e13f8b
Vic Frederick <vic79@me.com>
2020-03-10 10:30:12 -0500

    Update ProcessingCaller.py

--- Processings/v3/ProcessingCaller.py
+++ Processings/v3/ProcessingCaller.py
@@ -1,18 +1,36 @@
-# this 
-
+# this script will call FreqClusterer with # arguments
+# arg 0:	path to the labview generated tdms raw data file
+# arg 1: 	name of the tdms file
+# arg 2:	number of X pixels in spm text
+# arg 3:	number of Y pixels in spm text
+# arg 4: 	path to save created files
+# arg 5: 	name for created files (will be used for csv and video outputs)
+# arg 6:	test being ran (0=current, 1=photodetector)
+# arg 7: 	tested noise current
+# arg 8: 	expected SNR of current
+# arg 9: 	tested noise photodetector 1
+# arg 10: 	expected SNR of photodetector 1
+# arg 11: 	tested noise photodetector 2
+# arg 12: 	expected SNR of photodetector 2
 import sys
 import os.path
 
 numberOfExpectedArgs = 7
 arg = sys.argv[1:]
 if len(arg) = numberOfExpectedArgs:	
-	save_path = arg[0]
-	name_of_file = arg[1]
+	InputPath = arg[0]
+	InputName = arg[1]
 	Xpixels = arg[2]
 	Ypixels = arg[3]
-	SNRs = arg[4:]
-	completeName = os.path.join(save_path, name_of_file+".tdms")
-	FrequencyClusterer(completeName,Xpixels,Ypixels,SNRs)
+	outPath = arg[4]
+	outName = arg[5]
+	noise = arg[9] if arg[6] else arg[7]
+	SNR = arg[10] if arg[6] else arg[8]
+
+	InputTDMS = os.path.join(InputPath, InputName+".tdms")
+	OutputCSV = os.path.join(outPath, outName+".csv")
+	OutputMP4 = os.path.join(outPath, outName+".mp4")
+	FreqClusterer(InputTDMS,Xpixels,Ypixels,noise,SNR,OutputCSV,OutputMP4)
 else:
 	print("insufficent arguments to FrequencyClusterer.py")
 print("Processing caller is finished. Returning to graphic interface.")
