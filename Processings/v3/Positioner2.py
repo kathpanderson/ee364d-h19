8811ae678cfcd17cbe458ce30e0607839341333b
Vic Frederick <vic79@me.com>
2020-03-16 11:47:31 -0500

    Almost Production Ready

--- Processings/v3/Positioner2.py
+++ Processings/v3/Positioner2.py
@@ -8,47 +8,37 @@ import matplotlib.pyplot as plt
 # Inputs:	x = postions taken at the same time as the data
 #		  	pixelsX and pixelsY are the dementions of the test
 # Output:	list of tuples for the descrete positions
-def Positioner2(x,pixelsX,pixelsY):
+def Positioner2(x,pixelsX):
 	print("Start Positioning")
 	positions = []
 	xFiltered = FIRwindowFilter(x,4000000.0,5.0) # signal, sampling_rate(Hz) cutoff(Hz)
 	x = (x-np.min(x))
 	x = x*(pixelsX-1)/max(x)
 	x = [int(i) for i in x]
-	downsample = 50
+	downsample = 500
 	xDown = x[::downsample]
 	dx = np.diff(xDown)
 	dx = [0 if i < 0 else 1 for i in dx]
 	
-	y = np.zeros(len(x))
+	y = np.zeros(len(x), dtype=int)
 	prevSign = dx[0]
 	prevY = 0
+	i = 0
 	#print("len(dx): ",len(dx))
 	for i in range(len(dx)):
 		#print("LOOP")
 		if (dx[i] > prevSign):
-			#print("change")
+			#print("Last Y: ",prevY)
 			prevY = prevY+1
 			prevSign = dx[i]
 		if (dx[i] < prevSign):
 			prevSign = dx[i]
-		for j in range(downsample+len(x)%downsample):
+		for j in range(downsample):
 			y[j+downsample*i] = prevY
-	y = y*(pixelsY-1)/max(y)
-	y = [int(i) for i in y]
-	#plt.plot(y)
+	for k in range(len(x)%downsample):
+			y[k+downsample*(i+1)] = prevY
+	#plt.plot(y[::250])
 	#plt.show()
-	
+
 	positions = [x, y]
 	return positions
-	#print("x position max: ",np.max(x)," min: ", np.min(x))
-	#print("y position max: ",np.max(y)," min: ", np.min(y))
-	#print(positions[0:2])
-	
-	"""
-	print("x position max: ",np.max(x)," min: ", np.min(x))
-	
-	print("dx length: ",len(dx))
-	print("x length: ",len(x))
-	"""
-
