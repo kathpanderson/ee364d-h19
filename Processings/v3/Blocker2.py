fa8707b89f85458533e0120d7e48b92e47998910
Vic Frederick <vic79@me.com>
2020-03-16 11:46:45 -0500

    Almost Production Ready

--- Processings/v3/Blocker2.py
+++ Processings/v3/Blocker2.py
@@ -1,5 +1,4 @@
 import numpy as np
-from Positioner2 import Positioner2
 # function returns list BlockedData, a space-time seperated verstion of the data
 # highest list is x direction
 # next highest is y direction
@@ -8,25 +7,31 @@ from Positioner2 import Positioner2
 #		  	InData = value of data collected
 #		  	pixelsX and pixelsY are the dementions of the test
 # Output:	list of spacially shaped data
-def Blocker2(InPos, InData, pixelsX, pixelsY):
+def Blocker2(positions, InData, pixelsX, pixelsYMessured):
 	print("Start Blocking")
 	BlockedData = []
-	for x in range(0,pixelsX):
+	for x in range(pixelsX):
 		BlockedData.append([])
-		for y in range(0,pixelsY):
+		for y in range(pixelsYMessured):
 			BlockedData[x].append([])
 
-	positions = Positioner2(InPos,pixelsX,pixelsY)
 	for k in range(len(InData)):
+		#print(positions[0][k],positions[1][k],InData[k])
 		BlockedData[positions[0][k]][positions[1][k]].append(InData[k])
-		# Find the maximum length of a time series
-		maxLen = 0
-		for i in range(pixelsX):
-			for j in range(pixelsY):
-				if len(BlockedData[i][j]) > maxLen:
-					maxLen = len(BlockedData[i][j])
-		# zero pad all shorter time series
-		for i in range(pixelsX):
-			for j in range(pixelsY):
-					BlockedData[i][j].extend([0]*(maxLen-len(BlockedData[i][j])))
+	print("DONE WITH NON-Norm BLOCKING")
+	# Find the maximum length of a time series
+	maxLen = 0
+	for i in range(pixelsX):
+		maxLenNew = len(BlockedData[i][9])
+		if maxLenNew > maxLen: # choose arbitrary y for max len find
+			maxLen = maxLenNew
+	#print("maxLen: ", maxLen)
+	# zero pad all shorter time series
+	for i in range(pixelsX):
+		for j in range(pixelsYMessured):
+				dataLength = len(BlockedData[i][j])
+				if maxLen > dataLength:
+					BlockedData[i][j].extend([0]*(maxLen-dataLength))
+				elif maxLen < dataLength:
+					BlockedData[i][j] = BlockedData[i][j][:maxLen]
 	return BlockedData
