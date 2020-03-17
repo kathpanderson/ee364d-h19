c74d599b748b55ef474e7552d0abf907ce79d1ef
clarajohnson <clarajohnson986@utexas.edu>
2020-03-16 21:06:37 -0500

    changed BlockedData to numpy array

--- Processings/v3/Blocker2.py
+++ Processings/v3/Blocker2.py
@@ -1,4 +1,5 @@
 import numpy as np
+import sys
 # function returns list BlockedData, a space-time seperated verstion of the data
 # highest list is x direction
 # next highest is y direction
@@ -9,6 +10,7 @@ import numpy as np
 # Output:	list of spacially shaped data
 def Blocker2(positions, InData, pixelsX, pixelsYMessured):
 	print("Start Blocking")
+	# print("Maxsize: " + str(sys.maxsize))
 	BlockedData = []
 	for x in range(pixelsX):
 		BlockedData.append([])
@@ -25,13 +27,26 @@ def Blocker2(positions, InData, pixelsX, pixelsYMessured):
 		maxLenNew = len(BlockedData[i][9])
 		if maxLenNew > maxLen: # choose arbitrary y for max len find
 			maxLen = maxLenNew
-	#print("maxLen: ", maxLen)
+	print("maxLen: ", maxLen)
+
+	newArray = np.zeros((pixelsX,pixelsYMessured,maxLen), dtype=float32)
 	# zero pad all shorter time series
-	for i in range(pixelsX):
+	"""for i in range(pixelsX):
 		for j in range(pixelsYMessured):
 				dataLength = len(BlockedData[i][j])
+				print("DATALENGTH:" + str(i) + " " + str(j) + " " + str(dataLength))
 				if maxLen > dataLength:
-					BlockedData[i][j].extend([0]*(maxLen-dataLength))
+					# BlockedData[i][j].extend([0]*(maxLen-dataLength))
+					# print("ADD:" + str(i) + " " + str(j) + " " + str(maxLen-dataLength))
+					for e in range(maxLen-dataLength):
+						BlockedData[i][j].append(0)
 				elif maxLen < dataLength:
-					BlockedData[i][j] = BlockedData[i][j][:maxLen]
-	return BlockedData
+					BlockedData[i][j] = BlockedData[i][j][:maxLen]"""
+
+	for i in range(pixelsX):
+		for j in range(pixelsYMessured):
+			minLength = min(len(BlockedData[i][j]), maxLen)
+			for e in range(minLength):
+				newArray[i][j][e] = BlockedData[i][j][e]
+	print("done with blocking")
+	return newArray
