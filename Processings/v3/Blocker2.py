3ef8b7b8dae9add7501e83615555d9183c42349b
clarajohnson <clarajohnson986@utexas.edu>
2020-03-20 13:32:31 -0500

    Changed everything into a numpy array; still have to append InData at the end

--- Processings/v3/Blocker2.py
+++ Processings/v3/Blocker2.py
@@ -11,25 +11,44 @@ import sys
 def Blocker2(positions, InData, pixelsX, pixelsYMessured):
 	print("Start Blocking")
 	# print("Maxsize: " + str(sys.maxsize))
-	BlockedData = []
+	"""BlockedData = []
 	for x in range(pixelsX):
 		BlockedData.append([])
 		for y in range(pixelsYMessured):
-			BlockedData[x].append([])
+			BlockedData[x].append([])"""
+
+	lenArray = np.zeros((pixelsX, pixelsYMessured), dtype=int)
+
+	maxLen = 0
 
 	for k in range(len(InData)):
+		lenArray[positions[0][k]][positions[1][k]] = lenArray[positions[0][k]][positions[1][k]] + 1
+
+	# If you still want an arbitrary maxLen, this would be the for loop to do it in @Vic
+	for x in range(pixelsX):
+		for y in range(pixelsYMessured):
+			if(lenArray[x][y] > maxLen):
+				maxLen = lenArray[x][y]
+
+	"""for k in range(len(InData)):
 		#print(positions[0][k],positions[1][k],InData[k])
 		BlockedData[positions[0][k]][positions[1][k]].append(InData[k])
-	print("DONE WITH NON-Norm BLOCKING")
+	print("DONE WITH NON-Norm BLOCKING")"""
+
 	# Find the maximum length of a time series
-	maxLen = 0
+	"""maxLen = 0
 	for i in range(pixelsX):
 		maxLenNew = len(BlockedData[i][9])
 		if maxLenNew > maxLen: # choose arbitrary y for max len find
 			maxLen = maxLenNew
-	print("maxLen: ", maxLen)
+	print("maxLen: ", maxLen)"""
 
-	newArray = np.zeros((pixelsX,pixelsYMessured,maxLen), dtype=np.float32)
+	try:
+		BlockedData = np.zeros((pixelsX,pixelsYMessured,maxLen), dtype=np.float32)
+	except Exception as ex:
+		# Memory error. Will have to use file I/O
+		print(ex)
+
 	# zero pad all shorter time series
 	"""for i in range(pixelsX):
 		for j in range(pixelsYMessured):
@@ -43,10 +62,13 @@ def Blocker2(positions, InData, pixelsX, pixelsYMessured):
 				elif maxLen < dataLength:
 					BlockedData[i][j] = BlockedData[i][j][:maxLen]"""
 
-	for i in range(pixelsX):
+	"""for i in range(pixelsX):
 		for j in range(pixelsYMessured):
 			minLength = min(len(BlockedData[i][j]), maxLen)
 			for e in range(minLength):
-				newArray[i][j][e] = BlockedData[i][j][e]
+				newArray[i][j][e] = BlockedData[i][j][e]"""
+
+
+
 	print("done with blocking")
 	return newArray
